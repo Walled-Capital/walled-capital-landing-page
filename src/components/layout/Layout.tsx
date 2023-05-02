60f3ce4c9d788eda6deac7078a25da22a228a86e
samfisher7777 <artur.z@blaize.tech>
2023-05-02 14:34:32 +0300

    feature: added header

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react';
+import { Box, Flex, FlexProps, SystemStyleObject } from '@chakra-ui/react';
+
+import { Header } from '@src/components/layout/Header';
+import { Footer } from '@src/components/layout/Footer';
+
+const LayoutStyles: SystemStyleObject = {
+  flexDIr: 'column',
+  flexDir: 'column',
+  minH: '100%',
+  position: 'relative',
+  alignItems: 'center',
+};
+
+export const Layout: FC<FlexProps> = ({ children, ...props }) => (
+  <Flex sx={LayoutStyles} {...props} >
+    <Header />
+    <Box as="main" flex="1 1 auto">
+      {children}
+    </Box>
+    <Footer />
+  </Flex>
+);
